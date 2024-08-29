89b18464bbc3e2d641a71dba4175bf049437cae7
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-08-29 14:32:45 -0300

    middleware nas rotas para implementar a validação de permissões

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const { hasPermission } = require('../middleware/hasPermission');
+const { hasPermission } = require('./middleware/hasPermission');
 
 router.get('/', hasPermission(['ler_usuarios']), userController.findAll);
 router.get('/:id', hasPermission(['ler_usuarios']), userController.findById);
